a6d584890ecbdd31c988aaeac5e7edf0bccd7ac3
Yohan Gastoud <yohan.g99@gmail.com>
2022-05-17 15:28:09 +0200

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,27 @@
+
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.src =
+      "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
+  $("head").append(s);
+  var myCanvas = document.createElement("canvas");
+  myCanvas.style.position = "absolute";
+  myCanvas.style.top = 0;
+  myCanvas.style.right = 0;
+  myCanvas.style.left = 0;
+  myCanvas.style.bottom = 0;
+  myCanvas.style.zIndex = 10;
+  document.body.appendChild(myCanvas);
+  myCanvas.height = 1000;
+  myCanvas.width = 1000;
+  var myConfetti = confetti.create(myCanvas, {
+      resize: true,
+      useWorker: true,
+  });
+  myConfetti({
+      particleCount: 5000,
+      spread: 340,
+  });
+  setTimeout(() => {
+      myCanvas.style.zIndex = 0;
+  }, 3000);
